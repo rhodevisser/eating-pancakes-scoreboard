2b65de865cf719b51c310215c1c2e2dbed4128fd
kapufskipauw <rosarhode@gmail.com>
2025-10-22 17:08:45 +0200

    feat: Add JS to ALL buttons

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let homeScoreBtnOne = document.getElementById("home-score-btn-1")
 let homeScoreEl = document.getElementById("home-score")
 let homeScore = 0
+
+
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
 
 function increaseHomeScoreOne() {
     homeScore += 1
@@ -8,4 +10,47 @@ function increaseHomeScoreOne() {
 }  
 
 
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+
+function increaseHomeScoreTwo() {
+    homeScore += 2 
+    homeScoreEl.textContent = homeScore 
+}
+
+
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+
+function increaseHomeScoreThree() {
+    homeScore += 3 
+    homeScoreEl.textContent = homeScore 
+}
+
+
+
+let guestScoreEl = document.getElementById("guest-score")
+let guestScore = 0
+
+
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
+
+function increaseGuestScoreOne() {
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+
+
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
+
+function increaseGuestScoreTwo() {
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+}
+
+
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+
+function increaseGuestScoreThree() {
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+}
 
