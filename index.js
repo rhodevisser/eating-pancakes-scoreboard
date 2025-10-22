3b0d33ea1fda080dc5660c6f2329954f0c1e1452
kapufskipauw <rosarhode@gmail.com>
2025-10-22 12:24:41 +0200

    feat: Add function to button
    It adds one to home score screen

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ let homeScoreBtnOne = document.getElementById("home-score-btn-1")
 let homeScoreEl = document.getElementById("home-score")
 let homeScore = 0
 
+function increaseHomeScoreOne() {
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}  
+
+
+
